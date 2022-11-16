4df70d2c3341d9953fe54599c361dd281e31f2f3
Alan Phan <alanphanxyz@gmail.com>
2022-11-16 23:14:27 +0700

    stone 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBrvQDxhOm2YFo9R2f50GB3nb1oIAyKPzU',
+  authDomain: 'fir-9-20af2.firebaseapp.com',
+  projectId: 'fir-9-20af2',
+  storageBucket: 'fir-9-20af2.appspot.com',
+  messagingSenderId: '75430517222',
+  appId: '1:75430517222:web:32972a62f080c3212fde27',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
